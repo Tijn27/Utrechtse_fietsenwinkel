52133d3bf917a32f680b93c58ab3df8bf38e10b5
Mac Hogenboom <45262193+sedundnesye@users.noreply.github.com>
2019-11-28 11:27:19 +0100

    neef

--- home.php
+++ home.php
@@ -1,56 +1,65 @@
 <div class="row">
   <div class="col">
-    <a href="index.php?content=Producten"><img id="home_img" src="./img/Kopen.png" alt="Koop je fiets" href="Producten.php"></a>
+    <a id="home_img" href="index.php?content=Producten" style="background-image: url('./img/kopen.png'); background-size: cover;"></a>
   </div>
   <div class="col">
-    <a href="index.php?content=Reparatie"><img id="home_img" src="./img/fietsenmaker.jfif" alt="Plan een reparatie in"></a>
+    <a id="home_img" href="index.php?content=Reparatie" style="background-image: url('./img/fietsenmaker.jfif'); background-size: cover;"></a>
   </div>
 </div>
 <div class="row">
   <div class="col">
-    <a href="index.php?content=Proefrit"><img id="home_img" src="./img/proefrit.jfif" alt="Maak een proefrit"></a>
+    <a id="home_img" href="index.php?content=Proefrit" style="background-image: url('./img/proefrit.jfif'); background-size: cover;"></a>
   </div>
-  <a href="index.php?content=Contact"><div id="home_img" style="background-image: url('./img/locatie.PNG'); background-size: cover;" class="col"></a>
+  <div  class="col">
+  <a id="home_img" href="index.php?content=Contact" style="background-image: url('./img/locatie.PNG'); background-size: cover;"></a>
   </div>
 </div>
 
 
 <!-- carousel -->
-<div style="margin-top: 20px;" class="bd-example">
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+<div style="margin-top: 20px; margin-left:17.5px;" class="bd-example">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" style=" width:1075px;">
       <ol class="carousel-indicators">
         <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
         <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        <li date-target="#carouselExampleCaptions" data-slide-to="3"></li>
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="..." class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <img src="./img/fietsen voor edit/foto1.jpg" alt="..."style="height:350px; width:1075px">
+          <div class="carousel-caption d-none d-md-block" style="color:white;">
+            <h5>slide 1</h5>
+            <p>deze carousel is gemaakt door mac.</p>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="..." class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <img src="./img/stella.jpg" alt="..." style="height:350px; width:1075px">
+          <div class="carousel-caption d-none d-md-block" style="color:white;">
+            <h5>slide 2</h5>
+            <p>hij had hulp van zijn neef.</p>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="..." class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          <img src="./img/fietsen voor edit/foto2.jpg" alt="..."style="height:350px; width:1075px">
+          <div class="carousel-caption d-none d-md-block" style="color:white;">
+            <h5>slide 3</h5>
+            <p>die neef is ook een matie.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="./img/fietsen voor edit/foto3.jpg" alt="..." style="height:350px; width:1075px">
+          <div class="carousel-caption d-none d-md-block" style="color:white;">
+            <h5>slide 4</h5>
+            <p>die matie is een klasgenoot.</p>
           </div>
         </div>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev" style="background-color: black;">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
       </a>
-      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next" style="background-color: black;">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
       </a>
--- navbar.php
+++ navbar.php
@@ -1,5 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark" id="nav">
-  <a class="navbar-brand" href="index.php?content=home">Utrechtse fietsenwinkel</a>
+  <a class="navbar-brand" href="index.php?content=home"><img src="img/Logo3.png" width="250"
+    height="75" alt="Utrechtse Fietsenwinkel"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -9,16 +10,19 @@
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li> -->
       <li class="nav-item">
-        <a class="nav-link" id="active_producten" href="index.php?content=Producten">Producten</a>
-      </li>
+        <a class="nav-link" id="active_producten" href="index.php?content=Producten">Producten   </a> 
+      </li> 
+      &nbsp;
       <li class="nav-item">
-        <a class="nav-link" id="active_reparatie" href="index.php?content=Reparatie">Reparatie</a>
+        <a class="nav-link" id="active_reparatie" href="index.php?content=Reparatie">Reparatie    </a>
       </li>
+      &nbsp;
       <li class="nav-item">
-        <a class="nav-link" id="active_proefrit" href="index.php?content=Proefrit">Proefrit</a>
+        <a class="nav-link" id="active_proefrit" href="index.php?content=Proefrit">Proefrit       </a>
       </li>
+      &nbsp;
       <li class="nav-item">
-        <a class="nav-link" id="active_contact" href="index.php?content=Contact">Contact</a>
+        <a class="nav-link" id="active_contact" href="index.php?content=Contact">Contact          </a>
       </li>
     </ul>
   </div>
