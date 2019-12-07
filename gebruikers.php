7b54b0ea660cb761ba6b101882e6867d20784596
Tijn Smit <ionic.320625@gmail.com>
2019-12-07 18:44:28 +0100

    overzicht gebruikers

--- gebruikers.php
+++ gebruikers.php
@@ -1,13 +1,24 @@
-Hier woorden de gebruikers beheerd
+<h2>Gebruikers:</h2>
 <?php
 include("./fuctions/connect_db.php");
-$sql = "SELECT gebruikersnaam FROM gebruikers";
-$result = $conn->query($sql);
+//telt aantal gebruikers
+    // $sql = "SELECT COUNT(naam) FROM gebruikers;";
+    // $result = mysq;
+    // $data=mysql_fetch_assoc($result);
+    // echo $data['total'];
+    //
+    // $result=mysql_query("SELECT COUNT(naam) FROM gebruikers;");
+    // $data=mysql_fetch_assoc($result);
+    // echo $data['total'];
 
+
+$sql = "SELECT naam FROM gebruikers";
+$result = $conn->query($sql);
+// var_dump($result);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br> Gebruikersnaam: ". $row["gebruikersnaam"]. "<br>";
+        echo "<br> Gebruikersnaam: ". $row["naam"]. "<br>";
     }
 } else {
     echo "0 results";
