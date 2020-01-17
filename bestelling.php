dc3825848ebae74b41fc6d4e5db043ca686e9332
Tijn Smit <ionic.320625@gmail.com>
2020-01-17 12:52:21 +0100

    nu werkt het nog

--- bestelling.php
+++ bestelling.php
@@ -29,19 +29,15 @@ if(!isset($_COOKIE['winkelwagen'])) {
                     <p style='text-align: right;'>Aantal:</p>
                 </div>
                 <div class='col-sm'>
-                    <select class='custom-select' id='inputGroupSelect01'>
-                        <option value='1' "; if($_COOKIE[$value] == 1){echo "selected";} echo " >1</option>
-                        <option value='2' "; if($_COOKIE[$value] == 2){echo "selected";} echo " >2</option>
-                        <option value='3' "; if($_COOKIE[$value] == 3){echo "selected";} echo " >3</option>
-                        <option value='4' "; if($_COOKIE[$value] == 4){echo "selected";} echo " >4</option>
-                        <option value='5' "; if($_COOKIE[$value] == 5){echo "selected";} echo " >5</option>
-                        <option value='6' "; if($_COOKIE[$value] == 6){echo "selected";} echo " >6</option>
-                        <option value='7' "; if($_COOKIE[$value] == 7){echo "selected";} echo " >7</option>
-                        <option value='8' "; if($_COOKIE[$value] == 8){echo "selected";} echo " >8</option>    
-                        <option value='9' "; if($_COOKIE[$value] == 9){echo "selected";} echo " >9</option>
-                        <option value='10' "; if($_COOKIE[$value] == 10){echo "selected";} echo " >10</option>
-                        <option value=' "; if($_COOKIE[$value] > 10){echo $_COOKIE[$value];}echo" ' "; if($_COOKIE[$value] > 10){echo "selected";} echo " >"; if($_COOKIE[$value] > 10){echo $_COOKIE[$value];}else{echo "10+";} echo"</option>
-                    </select>
+                    <select class='custom-select' id='inputGroupSelect01'>";
+                    for($x=1; $x<=10; $x++){
+                        echo"<option value='$x' "; if($_COOKIE[$value] == $x){echo "selected";} echo " >$x</option>";
+                    }
+                    if($_COOKIE[$value] > 10){
+                    echo
+                        "<option value=' "; if($_COOKIE[$value] > 10){echo $_COOKIE[$value];}echo" ' "; if($_COOKIE[$value] > 10){echo "selected";} echo " >"; if($_COOKIE[$value] > 10){echo $_COOKIE[$value];}else{echo "10+";} echo"</option>";
+                    }
+                    echo "</select>
                 </div>
                 <form method='post' class='col-sm' action='./index.php?content=fuctions/product_verwijderen_script'>
                     <input type='hidden' value='$value' name='productCode'>
